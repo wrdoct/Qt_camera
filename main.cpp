6011b557906d50dbdde01869c00de19d159dd4b0
wrdoct <llw18729357726@163.com>
2021-12-25 17:02:27 +0800

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "widget.h"
+#include <QApplication>
+#include"loginwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //Widget w;
+    //w.show();
+
+    loginWindow login;
+
+    login.setWindowTitle("登录界面");
+    login.show();
+
+    //QObject::connect(&login, SIGNAL(showmain()), &w, SLOT(receiveLogin())); //第一个参数sender为发送对象；第二个参数为发送对象里面的一个信号；第三个参数为接收对象；第四个参数为接收对象里面的槽函数
+
+    return a.exec();
+}
