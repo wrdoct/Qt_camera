316333742c3cb5547ee0e73e87ed57f1df3af663
wrdoct <llw18729357726@163.com>
2021-12-25 19:21:05 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
 
     login.setWindowTitle("登录界面");
     login.show();
+	//test
 
     //QObject::connect(&login, SIGNAL(showmain()), &w, SLOT(receiveLogin())); //第一个参数sender为发送对象；第二个参数为发送对象里面的一个信号；第三个参数为接收对象；第四个参数为接收对象里面的槽函数
 
